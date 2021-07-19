c66394cf4c5f5b3751b775f4be53ad05c779efce
Andres Rubiano <andresfrm2210@gmail.com>
2021-07-18 22:25:14 -0500

    Corrected main flow

--- main.py
+++ main.py
@@ -1,23 +1,17 @@
 from extract import *
 from transform import *
 
-import xml.etree.ElementTree as ET
 
 
 if __name__ == "__main__":
-
+    download = False
     datasource = dataWits()
-    countries = datasource.getCountriesList()
-    #for country in countries:
-    #    years = datasource.getYearsAvailable(country['isoCode'])
-    #    print( country['name'], years)
-    #    for year in years:
-    #        datasource.exportValues(country,year)
-    #    break 
-    #datasource.exportValues('AFG', 2008)
-    #readAndMerge('AFG', 2008)
-
-    for i in existingDirectories('AFG'):
-        readAndMerge('AFG', i)
-        print(i, '\n')
+    if download:
+        datasource.downloadData()
     
+    countries = datasource.getCountriesList()   
+    #print(countries)   
+    data = totalData(countries)
+    
+    data_filtered = filterExportations(data, 2018)
+    saveRequestedData(data_filtered)
