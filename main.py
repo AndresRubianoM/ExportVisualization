3edcd08821e762808769efef3fe260d0ef62a4b1
Andres Rubiano <andresfrm2210@gmail.com>
2021-07-29 15:19:25 -0500

    add new data and new format to graph

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from extract import *
 from transform import *
 
-
-
 if __name__ == "__main__":
     download = False
+    updateFilter = False
     datasource = dataWits()
     if download:
         datasource.downloadData()
     
-    countries = datasource.getCountriesList()   
-    #print(countries)   
-    data = totalData(countries)
+    
     
     #data_filtered = filterTop(data, 2018)
-    data_filtered = relevantPartners(data, 2018)
-    saveRequestedData(data_filtered)
+    if updateFilter:
+        countries = datasource.getCountriesList()    
+        data = totalData(countries)
+        data_filtered = relevantPartners(data, 2018)
+        saveRequestedData(data_filtered)
+
+    data_graphHEB = prepareDataHBE()
+    saveRequestedData(data_graphHEB, "dataGraph")
